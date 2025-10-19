32c32b90d5ec1dc8d02e162765c66104f552af2a
leah-massey <15648177+leah-massey@users.noreply.github.com>
2025-10-19 12:12:07 +0100

    test: input validation for invalid user input length

--- src/test/kotlin/unit/InputValidationTest.kt
+++ src/test/kotlin/unit/InputValidationTest.kt
@@ -4,6 +4,7 @@ import InputValidation
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
 
 class InputValidationTest {
 
@@ -15,6 +16,12 @@ class InputValidationTest {
         fun `returns true when input of required length`() {
             val input = listOf("newaccount", "Coco", "Gauff")
             assertTrue(inputValidation.isValidInputLength(input, 3))
+        }
+
+        @Test
+        fun `returns false when input of required length`() {
+            val input = listOf("newaccount", "Coco", "Gauff", "Young")
+            assertFalse(inputValidation.isValidInputLength(input, 3))
         }
     }
 
