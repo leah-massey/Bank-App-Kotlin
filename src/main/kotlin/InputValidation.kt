4ba420bff13477ba20f2fdcb2501862ae4101aa6
leah-massey <15648177+leah-massey@users.noreply.github.com>
2025-10-19 21:10:23 +0100

    refactor: remove superseded code

--- src/main/kotlin/InputValidation.kt
+++ src/main/kotlin/InputValidation.kt
@@ -4,9 +4,4 @@ class InputValidation {
     fun isValidInputLength(input: List<String>, requiredLength: Int): Boolean {
         return (input.size == requiredLength)
     }
-
-    fun isValidCurrencyFormat(amount: String): Boolean {
-        return amount.toDoubleOrNull() != null
-    }
-
 }
--- src/test/kotlin/unit/InputValidationTest.kt
+++ src/test/kotlin/unit/InputValidationTest.kt
@@ -24,26 +24,4 @@ class InputValidationTest {
             assertFalse(inputValidation.isValidInputLength(input, 2))
         }
     }
-
-    @Nested
-    inner class IsValidAmount {
-        @Test
-        fun `returns true when amount has 2 decimal places` () {
-            val deposit = "10.00"
-            assertTrue(inputValidation.isValidCurrencyFormat(deposit))
-        }
-
-        @Test
-        fun `returns true when amount has no decimal places` () {
-            val deposit = "10"
-            assertTrue(inputValidation.isValidCurrencyFormat(deposit))
-        }
-
-        @Test
-        fun `returns false when amount is not a number`() {
-            val deposit = "10a"
-            assertFalse(inputValidation.isValidCurrencyFormat(deposit))
-        }
-    }
-
 }
