aa6c222103c768de373fef1f4569f935e53ccbb9
leah-massey <15648177+leah-massey@users.noreply.github.com>
2025-10-20 12:44:05 +0100

    refactor

--- src/main/kotlin/controller/BankingControllerImpl.kt
+++ src/main/kotlin/controller/BankingControllerImpl.kt
@@ -13,23 +13,23 @@ import ports.commands.WithdrawalCommand
 class BankingControllerImpl(val bankAccountService: BankAccountService, val userInput: UserInputProvider) :
     BankingController {
 
+    private val newAccountCommand = NewAccountCommand(bankAccountService)
+    private val depositCommand = DepositCommand(bankAccountService)
+    private val withdrawalCommand = WithdrawalCommand(bankAccountService)
+    private val balanceCommand = BalanceCommand(bankAccountService)
+    private val statementCommand = StatementCommand(bankAccountService)
+
+    private val commandMap: Map<String, Command> = mapOf(
+        "newaccount" to newAccountCommand,
+        "deposit" to depositCommand,
+        "withdraw" to withdrawalCommand,
+        "balance" to balanceCommand,
+        "statement" to statementCommand
+    )
+
     override fun startBanking() {
-        println("Welcome to the Bank CLI!")
-        println("NewAccount [FirstName] [LastName] -> creates new account")
 
-        val newAccountCommand = NewAccountCommand(bankAccountService)
-        val depositCommand = DepositCommand(bankAccountService)
-        val withdrawalCommand = WithdrawalCommand(bankAccountService)
-        val balanceCommand = BalanceCommand(bankAccountService)
-        val statementCommand = StatementCommand(bankAccountService)
-
-        var commandMap: Map<String, Command> = mapOf(
-            "newaccount" to newAccountCommand,
-            "deposit" to depositCommand,
-            "withdraw" to withdrawalCommand,
-            "balance" to balanceCommand,
-            "statement" to statementCommand
-        )
+        printInstructions()
 
         while (true) {
             print("> ")
@@ -46,5 +46,15 @@ class BankingControllerImpl(val bankAccountService: BankAccountService, val user
                 null -> if (command == "quit") break else println("I didn't quite get that, please try again")
             }
         }
+    }
+
+    private fun printInstructions() {
+        println("Welcome to the Bank CLI!")
+        println("NewAccount [FirstName] [LastName] -> creates new account")
+        println("Deposit [AccountId] [Amount]      -> Deposits funds into an account.")
+        println("Withdraw [AccountId] [Amount]     -> Withdraws funds from an account.")
+        println("Balance [AccountId]               -> Shows the current account balance.")
+        println("Statement [AccountId]             -> Prints the transaction history.")
+        println("Quit                              -> Quits the application.")
     }
 }
