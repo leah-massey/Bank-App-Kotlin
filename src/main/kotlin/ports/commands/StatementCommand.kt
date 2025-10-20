e6b7b73e969f4fea63253f35a766a5d05fbb3316
leah-massey <15648177+leah-massey@users.noreply.github.com>
2025-10-20 07:30:35 +0100

    refactor

--- src/main/kotlin/ports/commands/StatementCommand.kt
+++ src/main/kotlin/ports/commands/StatementCommand.kt
@@ -9,8 +9,13 @@ class StatementCommand(val bankAccountService: BankAccountService): Command {
         when (result) {
             is StatementSuccess -> {
                 result.message?.map { transaction ->
-                    println("${transaction.first}")
-                    println("balance: ${transaction.second}\n")
+                    if (transaction == result.message.first()) {
+                        println("account created")
+                        println("balance: ${transaction.second}\n")
+                    } else {
+                        println("${transaction.first}")
+                        println("balance: ${transaction.second}\n")
+                    }
                 }
             }
         }
